853cfdf74d74cf9c7b059cc3d2d8cf2ba7ec77ec
SIRANGU PURNIMA <sirangupurnima@gmail.com>
2026-06-26 20:10:06 +0530

    Use production backend URL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,8 +14,7 @@ api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
   }
 
